44c59b104cc7c283b1408fb8f24b3f46499d56af
EricKisee <erickiseemulwa@gmail.com>
2022-05-18 14:54:39 +0000

    Second Commit

--- src/main/java/com/erickisee/ec/repo/TourRepository.java
+++ src/main/java/com/erickisee/ec/repo/TourRepository.java
@@ -1,7 +1,59 @@
 package com.erickisee.ec.repo;
 
-import com.erickisee.ec.domain.*;
-import org.springframework.data.repository.CrudRepository;
+import com.erickisee.ec.domain.Tour;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
-public interface TourRepository extends CrudRepository<Tour, Integer> {
+
+/**
+ * Tour Repository Interface
+ *
+ * Created by Mary Ellen Bowman
+ */
+public interface TourRepository extends PagingAndSortingRepository<Tour, String> {
+    /**
+     * Find Tours associated with the Tour Package.
+     *
+     * @param code tour package code
+     * @return List of found tours.
+     */
+    Page<Tour> findByTourPackageCode(@Param("code")String code, Pageable pageable);
+
+    /**
+     * Only return the main fields of a Tour, not the details
+     *
+     * @param code tour package code
+     * @return tours without details
+     */
+    @Query(value = "{'tourPackageCode' : ?0 }",
+            fields = "{ 'id':1, 'title':1, 'tourPackageCode':1, 'tourPackageName':1}")
+    Page<Tour> findSummaryByTourPackageCode(@Param("code")String code, Pageable pageable);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> S save(S s);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> Iterable<S> saveAll(Iterable<S> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(String string);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Tour tour);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends Tour> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
--- src/main/java/com/erickisee/ec/service/TourService.java
+++ src/main/java/com/erickisee/ec/service/TourService.java
@@ -1,7 +1,5 @@
 package com.erickisee.ec.service;
 
-import com.erickisee.ec.domain.Difficulty;
-import com.erickisee.ec.domain.Region;
 import com.erickisee.ec.domain.Tour;
 import com.erickisee.ec.domain.TourPackage;
 import com.erickisee.ec.repo.TourPackageRepository;
@@ -9,12 +7,13 @@ import com.erickisee.ec.repo.TourRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 /**
  * Tour  Service
  *
  * Created by Mary Ellen Bowman
  */
-
 @Service
 public class TourService {
     private TourRepository tourRepository;
@@ -27,30 +26,18 @@ public class TourService {
     }
 
     /**
-     * Create a new Tour Object and persist it to the Database.
+     * Create a new Tour Object and persist it to the Database
      *
-     * @param title
-     * @param description
-     * @param blurb
-     * @param price
-     * @param duration
-     * @param bullets
-     * @param keywords
-     * @param tourPackageName
-     * @param difficulty
-     * @param region
-     * @return Tour Entity
+     * @param title Title of the tour
+     * @param tourPackageName tour Package of the tour
+     * @param details Extra details about the tour
+     * @return Tour
      */
-    public Tour createTour(String title, String description, String blurb, Integer price,
-                           String duration, String bullets,
-                           String keywords, String tourPackageName, Difficulty difficulty, Region region ) {
-        TourPackage tourPackage = tourPackageRepository.findById(tourPackageName).orElseThrow(()->
-             new RuntimeException("Tour package does not exist: " + tourPackageName));
-
-        return tourRepository.save(new Tour(title, description,blurb, price, duration,
-                bullets, keywords, tourPackage, difficulty, region));
+    public Tour createTour(String title, String tourPackageName, Map<String, String> details) {
+        TourPackage tourPackage = tourPackageRepository.findByName(tourPackageName).orElseThrow(() ->
+                new RuntimeException("Tour package does not exist: " + tourPackageName));
+        return tourRepository.save(new Tour(title, tourPackage, details));
     }
-
     /**
      * Calculate the number of Tours in the Database.
      *
@@ -59,6 +46,5 @@ public class TourService {
     public long total() {
         return tourRepository.count();
     }
-
 }
 
